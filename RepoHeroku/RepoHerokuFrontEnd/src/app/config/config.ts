3143ff7f99f8ec0e5d9c4503fb69230efd0f3adb
marcelo <equipostocker2020@gmail.com>
2020-07-14 09:51:30 -0300

    cambio url de conf por deploy backend heroku

--- RepoHeroku/RepoHerokuFrontEnd/src/app/config/config.ts
+++ RepoHeroku/RepoHerokuFrontEnd/src/app/config/config.ts
@@ -1,3 +1,3 @@
 // se genera una constante teniendo en cuenta las veces que sera necesario utilizar dentro de la app esta url
 // escalando la posibilidad de que cuando se pase a produccion siemplemente se cambia esta url aqui
-export const URL_SERVICIOS = 'http://localhost:3000' ;
+export const URL_SERVICIOS = 'https://prueba-stocker.herokuapp.com' ;
